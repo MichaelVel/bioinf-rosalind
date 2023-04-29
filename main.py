2f89727ac36a3cb2b8f0faf1def01bf200586b48
sebastian <msvelandiag@unal.edu.co>
2023-04-28 19:11:31 -0500

    restructure client utils library

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import typer
 
-import client.utilities as cl
-from client.tests import Test
+import client as cl
 
 app = typer.Typer()
 
@@ -12,13 +11,13 @@ def create(
         tmpl: cl.Template = typer.Option(cl.Template.simple, "--template", "-t")
 ):
     to_path = f"{cl.Location.package()}/{loc.module()}/{id}.py"
-    cl.copy_template(tmpl, to_path)
+    cl.io.copy_template(tmpl, to_path)
     print(f"Created file: {to_path}")
 
 
 @app.command()
 def get(source: str, file: str) -> str:
-    process_file = cl.get_data_from_source(source)
+    process_file = cl.utils.get_data_from_source(source)
     return process_file(file)
 
 @app.command()
@@ -33,11 +32,11 @@ def run(
     if source: 
         input = get(source, input)
 
-    solution = cl.load_solution_module(id, loc)
-    data = cl.parseInput(input)
+    solution = cl.utils.load_solution_module(id, loc)
+    data = cl.io.parseInput(input)
     solution = solution.main(data)
     if save:
-      cl.writeCache(f"solutions/{id}.txt", solution)
+      cl.io.writeCache(f"solutions/{id}.txt", solution)
 
     print(solution)
 
@@ -49,7 +48,7 @@ def test(
         loc: cl.Location = typer.Option(cl.Location.STRONGHOLD, "--location", "-l"),
         remove: bool = typer.Option(False, "--remove", "-r", help= test_help_remove),
 ):
-    test = Test(id,loc)
+    test = cl.tests.Test(id,loc)
     if remove:
         test.test_remove()
 
